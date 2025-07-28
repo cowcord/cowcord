fb3fab67eca2b3943377fbeb6a93db3b91c3aa05
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-07-28 02:50:16 -0700

    update `to_string_query` to be a method
    will update where its used too but im already rewriting all the endpoint files and im not fishing the query changes out of that mess

---
+++ crates/discord-types/src/utils/url.rs
@@ -0,0 +1,34 @@
+/// Convert struct to url string query params.
+///
+/// cannot convert types like Vec or Tuples.
+///
+/// Errors will be hidden and will just return an empty string.
+///
+/// eg.
+///
+/// with field(s)
+/// ```rust
+/// let query = MyStruct {
+///     value: "hi",
+///     value_two: None,
+///     value_three: 25
+/// }
+///
+/// assert_eq!(to_string_query(&query), "?value=hi&value_three=25");
+/// ```
+/// without fields
+/// ```rust
+/// let query = MyStruct {}
+///
+/// assert_eq!(to_string_query(&query), "");
+/// ```
+pub trait ToStringQuery: serde::Serialize {
+	fn to_string_query(&self) -> String {
+		serde_urlencoded::to_string(self)
+			.map(|q| format!("?{}", q))
+			.unwrap_or_default()
+	}
+}
+
+// implements the method for anything that derives `serde::ser::Serialize`
+impl<T> ToStringQuery for T where T: serde::ser::Serialize {}
