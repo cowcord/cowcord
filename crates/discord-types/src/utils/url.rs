5edcf32611e76f81febf82978d2372e192bb847e
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-07-28 23:04:46 -0700

    fix to_string_query doc comment

--- crates/discord-types/src/utils/url.rs
+++ crates/discord-types/src/utils/url.rs
@@ -1,28 +1,28 @@
-/// Convert struct to url string query params.
-///
-/// cannot convert types like Vec or Tuples.
-///
-/// Errors will be hidden and will just return an empty string.
-///
-/// eg.
-///
-/// with field(s)
-/// ```rust
-/// let query = MyStruct {
-///     value: "hi",
-///     value_two: None,
-///     value_three: 25
-/// }
-///
-/// assert_eq!(to_string_query(&query), "?value=hi&value_three=25");
-/// ```
-/// without fields
-/// ```rust
-/// let query = MyStruct {}
-///
-/// assert_eq!(to_string_query(&query), "");
-/// ```
 pub trait ToStringQuery: serde::Serialize {
+	/// Convert struct to url string query params.
+	///
+	/// cannot convert types like Vec or Tuples.
+	///
+	/// Errors will be hidden and will just return an empty string.
+	///
+	/// eg.
+	///
+	/// with field(s)
+	/// ```rust
+	/// let query = MyStruct {
+	///     value: "hi",
+	///     value_two: None,
+	///     value_three: 25
+	/// }
+	///
+	/// assert_eq!(&query.to_string_query(), "?value=hi&value_three=25");
+	/// ```
+	/// without fields
+	/// ```rust
+	/// let query = MyStruct {}
+	///
+	/// assert_eq!(query.to_string_query(), "");
+	/// ```
 	fn to_string_query(&self) -> String {
 		serde_urlencoded::to_string(self)
 			.map(|q| format!("?{}", q))
