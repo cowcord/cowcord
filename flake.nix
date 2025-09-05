2ea493a9548da35fa96e53b045150c5b630c6ea8
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-09-05 10:13:16 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+        in
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            packages = [
+              dioxus-cli
+
+            openssl
+            pkg-config
+
+              gtk3
+              libayatana-appindicator
+              webkitgtk_4_1
+              glib
+              pango
+              gdk-pixbuf
+              cairo
+              atkmm
+              libsoup_3
+              pkg-config
+            ];
+          };
+        }
+      );
+}
