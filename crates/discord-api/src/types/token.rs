331984f74f897ff40197bc9873e4a333a298148f
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2026-02-11 03:52:40 -0800

    make `Token::is_valid` actually check if its valid

--- crates/discord-api/src/types/token.rs
+++ crates/discord-api/src/types/token.rs
@@ -1,9 +1,29 @@
+use base64::Engine;
+use base64::prelude::BASE64_STANDARD;
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct Token(pub String);
 
 impl Token {
 	pub fn is_valid(&self) -> bool {
-		self.0.len() == 70
+		let mut sections = self.0.split(".");
+
+		// decode first from base64
+		// then cast the string to a int
+		// if all goes well then assume its a valid snowflake
+		// todo: when UserId type is added, use a `is_valid` for that instead of just assuming any number is fine
+		let section1_valid = sections
+			.next()
+			.and_then(|s| BASE64_STANDARD.decode(s).ok())
+			.and_then(|decoded| String::from_utf8(decoded).ok())
+			.and_then(|s| s.parse::<u32>().ok())
+			.is_some();
+
+		// verify theres exactly 2 more non-empty sections
+		section1_valid
+			&& sections.next().is_some_and(|s| !s.is_empty())
+			&& sections.next().is_some_and(|s| !s.is_empty())
+			&& sections.next().is_none()
 	}
 }
 
